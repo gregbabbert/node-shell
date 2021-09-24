b92025058aa06cde6ef9b876e0f0bbd59f12a99e
Greg <gdbabbert@gmail.com>
2021-09-24 15:26:25 -0400

    make ls file and commands

--- bash.js
+++ bash.js
@@ -1,10 +1,7 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
+pwd()
+ls()
 
 
-process.stdout.write('prompt > ');
-
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-
-  process.stdout.write(process.cwd() + cmd);
-  process.stdout.write('\nprompt > ')
-})
